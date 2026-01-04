3db8375cbc98198a7ad11d1cfdef9792db1031f6
Nils R <59091869+dark-barbarian@users.noreply.github.com>
2026-01-04 03:03:53 +0100

    Enhance TODO comment

--- main.py
+++ main.py
@@ -161,4 +161,4 @@ if __name__ == "__main__":
         logger.exception("Fatal error in outer run loop!")
         sys.exit(1)
 
-# TODO: implement scheduling stuff (wiki), add command: list observed/manual pages
+# TODO: implement scheduling stuff (wiki), add command: list observed/manual pages, schedule_post deferren, wenn mehrere Bilder angehängt werden
